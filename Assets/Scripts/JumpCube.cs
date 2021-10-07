f601f47ecd129cee03711d046f5bdda64ce4423d
Konrad Entner <ko.fcbu@gmail.com>
2021-10-07 13:42:51 +0200

    Jumping Cube

---
+++ Assets/Scripts/JumpCube.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpCube : MonoBehaviour
+{
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            rb.AddForce(new Vector3(0f, 10f, 0f), ForceMode.Impulse);
+    }
+}
